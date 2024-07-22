8efd6cd593930a0a2697491ace8b2d2d0930f6c4
Gauravraj45768 <chitu243546@gamil.com>
2024-07-23 02:21:22 +0530

    react state

--- basicreact/src/App.js
+++ basicreact/src/App.js
@@ -1,9 +1,23 @@
+import { useState } from "react";
 
 
 function App() {
+  const [count, setCount] = useState(0);
+  const increment = () => {
+   // setCount(count + 1);
+    setCount((precount) => precount + 1)
+    setCount((precount)=> precount+1)
+  }  
+  const decrement = () => {
+    setCount(count - 1);
+     setCount(count - 1);
+    }
   return (
     <div className="App">
       <h1>app start</h1>
+      <div>        
+        <button onClick={increment}>inc</button> {count} <button onClick={decrement}>dec</button>
+      </div>
     </div>
   );
 }
