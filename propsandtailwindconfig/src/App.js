3f7654750ae49a6cee65d083bdd0b34cc7173a5c
Gauravraj45768 <chitu243546@gamil.com>
2024-07-28 00:34:31 +0530

    react props

--- propsandtailwindconfig/src/App.js
+++ propsandtailwindconfig/src/App.js
@@ -5,9 +5,15 @@ import Card from './components/Card';
 function App() {
   return (
     <>
-     <h1 classname='bg-gray-400 text-black p-4 rounded-xl mb-4'>tailwind test</h1>
-      <Card />
-      
+      <h1 className="flex justify-center  bg-green-400 text-black p-4 rounded  mb-4  ">
+        Employee details
+      </h1>
+      <div className="flex  justify-around">
+        <Card username="alisa" />
+        <Card username="john" />
+        <Card username="jane" />
+        <Card />
+      </div>
     </>
   );
 }
--- propsandtailwindconfig/src/components/Card.js
+++ propsandtailwindconfig/src/components/Card.js
@@ -1,5 +1,5 @@
 
-function Card() {
+function Card({username="ayushi"}) {
     return (
       <>
        
@@ -11,7 +11,7 @@ function Card() {
           />
           <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
           <div className="absolute bottom-4 left-4 text-left">
-            <h1 className="text-lg font-semibold text-white">Delba</h1>
+            <h1 className="text-lg font-semibold text-white">{username}</h1>
             <p className="mt-2 text-sm text-gray-300">
               I am a software developer with a passion for creating innovative
               solutions. I have a strong background in programming languages
