b63ea2d4e9a9afbd466eee80a9a8df237df4729b
Gauravraj45768 <chitu243546@gamil.com>
2024-07-27 23:53:42 +0530

    react card

--- propsandtailwindconfig/src/App.js
+++ propsandtailwindconfig/src/App.js
@@ -1,13 +1,14 @@
 
-import './App.css';
+
 import Card from './components/Card';
 
 function App() {
   return (
     <>
-      <h1 className='text-3xl bg-lime-200'>hello world</h1>
-      <Card/>
-   </>
+     <h1 classname='bg-gray-400 text-black p-4 rounded-xl mb-4'>tailwind test</h1>
+      <Card />
+      
+    </>
   );
 }
 
--- propsandtailwindconfig/src/components/Card.js
+++ propsandtailwindconfig/src/components/Card.js
@@ -1,15 +1,33 @@
 
 function Card() {
     return (
-      <div className="px-60">
-        <img
-          src="https://cdn.pixabay.com/photo/2023/06/19/16/02/beautiful-woman-8074997_1280.jpg"
-          alt=""
-        />
-        <h1 className="text-2xl bg-green-500 p-3 rounded">a card for photos</h1>
+      <>
+       
+        <div className=" relative h-[400px] w-[300px] rounded-md ">
+          <img
+            src="https://cdn.pixabay.com/photo/2023/06/19/16/02/beautiful-woman-8074997_1280.jpg"
+            alt=""
+            className="z-0 h-full w-full rounded-md object-cover"
+          />
+          <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+          <div className="absolute bottom-4 left-4 text-left">
+            <h1 className="text-lg font-semibold text-white">Delba</h1>
+            <p className="mt-2 text-sm text-gray-300">
+              I am a software developer with a passion for creating innovative
+              solutions. I have a strong background in programming languages
+              such as JavaScript, Python, and C++. I am also experienced in
+              working with various frameworks and libraries, including React,
+              Angular, and Vue.js. I am a quick learner and always looking to
+              improve my skills and knowledge.
+            </p>
+            <button className="bg-green-400 p-2 mt-2 inline-flex cursor-pointer items-center text-sm font-semibold">
+              view profile
+            </button>
+          </div>
+        </div>
 
         <p></p>
-      </div>
+      </>
     ); 
 }
 export default Card;
